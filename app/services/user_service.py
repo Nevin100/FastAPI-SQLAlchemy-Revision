2dc2d2553020924493e25629293ad393640c7dfd
Nevin100 <opopopopop100000@gmail.com>
2026-01-08 22:43:35 +0530

    Pushing the PUT logic

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -61,6 +61,18 @@ def delete_user(db: Session, user_id: int):
     db.commit()
     return existing_user
 
-#
+# Update User : 
+def update_user(db: Session, user_id: int, user: UserCreate):
+    # Check whther the user exists : 
+    existing_user = db.query(User).filter(User.id == user_id).first()
 
+    if not existing_user:
+        return None
     
+    existing_user.name = user.name
+    existing_user.email = user.email
+
+    db.commit()
+    db.refresh(existing_user)
+
+    return existing_user
